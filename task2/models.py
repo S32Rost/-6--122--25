d505b8e65aa96f89a3f5bceb1f4d42e4acd89992
Шитый Ростслав Вячеславович <shityy.rostislav@gmail.com>
2026-06-08 21:46:12 +0300

    feat: реализовать in-memory базу данных с таблицей Students и консольным интерфейсом

---
+++ task2/models.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any
+
+@dataclass
+class Student:
+    id: int
+    name: str
+    age: int
+    major: str      # специальность
+    enrolled_at: datetime
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "major": self.major,
+            "enrolled_at": self.enrolled_at.isoformat()
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> "Student":
+        return cls(
+            id=data["id"],
+            name=data["name"],
+            age=data["age"],
+            major=data["major"],
+            enrolled_at=datetime.fromisoformat(data["enrolled_at"])
+        )
